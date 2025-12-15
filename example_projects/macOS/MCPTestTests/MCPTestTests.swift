e827aa935abaf68abf5e1f27eafe1d2dd0763f5f
Cameron Cooke <web@cameroncooke.com>
2025-12-15 13:48:48 +0000

    Add unit tests to macOS app example

---
+++ example_projects/macOS/MCPTestTests/MCPTestTests.swift
@@ -0,0 +1,16 @@
+//
+//  MCPTestTests.swift
+//  MCPTestTests
+//
+//  Created by Cameron on 15/12/2025.
+//
+
+import Testing
+
+struct MCPTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
